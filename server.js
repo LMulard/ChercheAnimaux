e06325bcb284bbf39b73add9113481b1c274e287
Luca Di Carlo <ldc.luca.dicarlo@gmail.com>
2018-06-08 11:00:31 +0200

    test index.html

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname +'/dist/ChercheAnimaux'));
-
+//app.use(express.static(__dirname +'/dist/ChercheAnimaux'));
+//app.use(express.static('index.html'));
 app.get('/*', function(req,res) {
     
-res.sendFile(__dirname +'/dist/ChercheAnimaux/index.html');
+//res.sendFile(__dirname +'/dist/ChercheAnimaux/index.html');
+//});
+res.sendFile(__dirname +'/index.html');
 });
-
 
 app.listen(process.env.PORT || 8080);
