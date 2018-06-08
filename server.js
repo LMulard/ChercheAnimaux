c59e9d8e842c4a5827c50285272a056840578f29
Luca Di Carlo <ldc.luca.dicarlo@gmail.com>
2018-06-08 10:21:51 +0200

    console log

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
+console.log("INIT");
 const app = express();
 
 // Serve only the static files form the dist directory
@@ -11,6 +11,6 @@ app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'./dist/ChercheAnimaux/index.html'));
 });
-
+console.log(process.env.PORT);
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
