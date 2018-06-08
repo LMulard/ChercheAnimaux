39a6a755a018ba1ed133d186b4d2226c01476669
Luca Di Carlo <ldc.luca.dicarlo@gmail.com>
2018-06-08 11:06:15 +0200

    re test

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/*', function(req,res) {
     
 //res.sendFile(__dirname +'/dist/ChercheAnimaux/index.html');
 //});
-res.sendFile(__dirname +'/index.html');
+res.sendFile(__dirname +'/dist/ChercheAnimaux/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
