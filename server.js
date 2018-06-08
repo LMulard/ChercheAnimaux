f0ee4a20bc9d996415a48f324dbc0645addb275a
Luca Di Carlo <ldc.luca.dicarlo@gmail.com>
2018-06-08 10:53:31 +0200

    port and files edited

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-console.log("INIT");
+
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + './dist/ChercheAnimaux'));
+app.use(express.static(__dirname +'/dist/ChercheAnimaux'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'./dist/ChercheAnimaux/index.html'));
+res.sendFile(__dirname +'/dist/ChercheAnimaux/index.html');
 });
-console.log(process.env.PORT);
-// Start the app by listening on the default Heroku port
+
+
 app.listen(process.env.PORT || 8080);
